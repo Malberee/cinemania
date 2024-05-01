285893fdbabcd0a0300b5cd268294758d0d2f01e
Malberee <82259650+Malberee@users.noreply.github.com>
2024-05-01 15:08:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import App from 'components/App.tsx'
 import { Provider } from 'react-redux'
 import store from 'store/store.ts'
 import ThemeProvider from 'components/ThemeProvider'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cinemania/">
+    <HashRouter basename="/cinemania/">
       <ThemeProvider>
         <Provider store={store}>
           <App />
         </Provider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashR>
   </React.StrictMode>
 )
